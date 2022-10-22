dfd808dd4b7fb24a352e413772a7d71c8a59678c
yikuang <yikuang@synthesis.partners>
2022-10-22 22:02:53 +0800

    init(ingestion-backend): package configs

---
+++ ion-ingestion/setup.py
@@ -0,0 +1,46 @@
+import os
+import re
+import pathlib
+import pkg_resources
+from setuptools import setup, find_packages
+
+name = "ion"
+req_file = "requirements.dev.txt"
+here = pathlib.Path.absolute(pathlib.Path(__file__).resolve().parent)
+
+# get package version
+with open(pathlib.Path(here, "src/ion/__init__.py"), encoding="utf-8") as f:
+    result = re.search(r'__version__ = ["\']([^"\']+)', f.read())
+
+    if not result:
+        raise ValueError("Can't find the version in ion/__init__.py")
+
+    version = result.group(1)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with pathlib.Path(os.path.abspath("."), req_file).open() as requirements:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements)
+    ]
+
+package_data = {"ion": ["*.txt", "*.json", "*.preamble", "*.sql"]}
+
+setup(
+    name=name,
+    version=version,
+    author="Loh Yi Kuang",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages("src", exclude=["*tests"]),
+    package_data=package_data,
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": ["ion = ion.launchers.cli:cli"],
+    },
+    python_requires=">=3.9",
+)
