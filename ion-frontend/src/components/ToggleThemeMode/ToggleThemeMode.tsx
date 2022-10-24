859ad7a7261763c4948a77098ab8277692f02874
yikuang <yikuang@synthesis.partners>
2022-10-24 12:11:05 +0800

    initialise cra frontend Fixes #1

---
+++ ion-frontend/src/components/ToggleThemeMode/ToggleThemeMode.tsx
@@ -0,0 +1,27 @@
+import IconButton from '@mui/material/IconButton';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { FC } from 'react';
+import { useThemeStore } from 'store/theme';
+
+function ToggleThemeMode() {
+  const { mode, setMode } = useThemeStore();
+
+  const handleClick = () => {
+    setMode();
+  };
+
+  if (!mode) return null;
+
+  return (
+    <IconButton aria-label="delete" onClick={handleClick}>
+      {mode === 'dark' ? (
+        <LightModeIcon data-testid="theme-mode-light-id" />
+      ) : (
+        <DarkModeIcon data-testid="theme-mode-dark-id" />
+      )}
+    </IconButton>
+  );
+};
+
+export default ToggleThemeMode;
