07e9eac57cde886b2986d1544c8e50e5a599a586
Yi Kuang <58301316+lvxhnat@users.noreply.github.com>
2022-11-06 16:44:32 +0800

    feat(frontend): Added market clock (#26)
    Co-authored-by: yikuang <yikuang@synthesis.partners>

---
+++ ion-frontend/src/components/Clock/Clock.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+import moment from 'moment-timezone'
+
+import { Grid } from '@mui/material';
+
+export default function MarketStatus() {
+
+    const [date, setDate] = useState<string>("");
+    const [marketStatus, setMarketStatus] = useState<string | undefined>();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            moment.tz.setDefault("America/New_York")
+            var newYorkTime = moment()
+            setDate(newYorkTime.format('DD MMM YY, HH:mm:ss ddd'))
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <Grid container alignItems='center'>
+            <span> {marketStatus} </span>
+            <span><b> NYT: </b> {date} </span>
+        </Grid >
+    )
+}
