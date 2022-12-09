8ec23dc9dd7328a931b85a2369a10af583bbfbf6
yikuang <yikuang@synthesis.partners>
2022-12-10 00:37:52 +0800

    fix: bulk insert w create on treasury

--- data-engine/tasks/usgov/treasury.py
+++ data-engine/tasks/usgov/treasury.py
@@ -22,21 +22,32 @@ def ingest_treasury(year: TreasuryYears, treasury_type: TreasuryTypes):
 @task
 def write_treasury(treasury_info: TreasuryInfoDTO):
     with postgres.session_scope() as session:
-        if inspect(postgres_engine).has_table(USTreasuryYield.__tablename__):
-            USTreasuryYield.__table__.drop(postgres_engine)
-        USTreasuryYield.__table__.create(postgres_engine)
-        session.bulk_save_objects(
-            [
-                USTreasuryYield(uuid=str(uuid.uuid4()), **object)
-                for object in treasury_info
-            ]
-        )
+        # Create table if it does not exist
+        if not inspect(postgres_engine).has_table(
+            USTreasuryYield.__tablename__
+        ):
+            USTreasuryYield.__table__.create(postgres_engine)
+            # Bulk insert if the table does not exist
+            session.bulk_save_objects([USTreasuryYield(uuid=str(uuid.uuid4()), **object)for object in treasury_info])
+        else:
+            # Iterate
+            for object in treasury_info:
+                if (
+                    session.query(USTreasuryYield.uuid)
+                    .filter_by(_date=object["_date"])
+                    .first()
+                    is not None
+                ):
+                    return
+                session.add(USTreasuryYield(uuid=str(uuid.uuid4()), **object))
 
 
 @flow
 def treasury_ingestion_flow():
-    result = ingest_treasury(2022, "YIELD_CURVE")
-    return write_treasury(result)
+    treasury_item = ingest_treasury.submit(2022, "YIELD_CURVE").result()
+    write_treasury.submit(treasury_item).result()
 
 
 treasury_ingestion_flow()
+
+        
