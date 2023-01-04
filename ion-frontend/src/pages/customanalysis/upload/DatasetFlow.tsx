d7953cd8084af8212ed9bc99861e92f21f9144c2
yikuang <yikuang@synthesis.partners>
2023-01-04 11:13:19 +0800

    edit: push

--- ion-frontend/src/pages/customanalysis/upload/DatasetFlow.tsx
+++ ion-frontend/src/pages/customanalysis/upload/DatasetFlow.tsx
@@ -5,14 +5,14 @@ import 'reactflow/dist/style.css';
 const initialNodes = [
     {
         id: '1',
-        data: { label: 'Hello' },
-        position: { x: 0, y: 0 },
+        data: { label: 'iteration.csv' },
+        position: { x: 100, y: 75 },
         type: 'input',
     },
     {
         id: '2',
         data: { label: 'World' },
-        position: { x: 100, y: 100 },
+        position: { x: 500, y: 75 },
     },
 ];
 
