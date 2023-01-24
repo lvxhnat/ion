25ac12f4f1152c51c839e80e98bcba81f61eb2c5
yikuang <yikuang@synthesis.partners>
2023-01-24 12:48:51 +0800

    init: rust-python mod dec

--- polar/polar/src/main.rs
+++
@@ -1,9 +0,0 @@
-mod expected_returns;
-
-fn main() {
-    println!("Hello, world!");
-    let mut test_vector: Vec<f32> = Vec::new();
-    test_vector.push(2.3);
-    test_vector.push(3.4);
-    expected_returns::returns_from_prices(test_vector, true);
-}
