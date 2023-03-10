967183532681fd9dcd3cfe18007b9dc0e04654b6
yikuang <yikuang@synthesis.partners>
2023-03-10 23:09:23 +0800

    edit

---
+++ ion-frontend/src/functions/watchlist/widget/widget.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import * as d3 from 'd3';
+
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+
+import BaseLineChart from 'components/Charting/BaseChart';
+import { DefaultDataProps } from 'components/Charting/BaseChart/schema/schema';
+import WidgetContainer from 'components/WidgetContainer';
+import { StyledTableCell, StyledChartCell } from 'components/Tables/BaseTable/StyledTableCell';
+import { TableCellWrapper } from 'components/Tables/BaseTable/style';
+import { ColorsEnum } from 'common/theme';
+import { StyledTableRow } from 'components/Tables/BaseTable/StyledTableRow';
+import { getETFCandles } from 'data/ingestion/candles';
+import { FinnhubCandlesSchema, FinnhubCandlesEntrySchema } from 'data/schema/candles';
+
+export default function Widget() {
+    const tickers = ['SPY', 'GPS', 'BABA', 'AAPL', 'TSLA', 'IAU', 'VGLT'];
+    const [tickerData, setTickerData] =
+        React.useState<
+            {
+                ticker: string;
+                chartData: DefaultDataProps;
+                last_close: string;
+                pct_change: number;
+            }[]
+        >();
+
+    React.useEffect(() => {
+        getETFCandles(tickers).then(res => {
+            setTickerData(
+                res.data.map((entry: FinnhubCandlesSchema) => {
+                    return {
+                        ticker: entry[0].symbol,
+                        chartData: {
+                            id: entry[0].symbol,
+                            name: entry[0].symbol,
+                            parent: true,
+                            dataX: entry.map(
+                                (obj: FinnhubCandlesEntrySchema) => new Date(obj.date)
+                            ),
+                            dataY: entry.map((obj: FinnhubCandlesEntrySchema) => obj.close),
+                            color: 'white',
+                            type: 'pureLine',
+                        },
+                        last_close: entry[entry.length - 1].close.toFixed(2),
+                        pct_change:
+                            (100 * ((entry[entry.length - 1].close - entry[entry.length - 2].close) /
+                            entry[entry.length - 2].close)),
+                    };
+                })
+            );
+        });
+    }, []);
+
+    return (
+        <WidgetContainer title="ticker_watchlist">
+            <Table style={{ minWidth: 150 }} aria-label="a dense table" stickyHeader>
+                <TableHead>
+                    <TableRow sx={{ backgroundColor: ColorsEnum.coolgray8 }}>
+                        <StyledTableCell isHeader key={`ticker_header_0`}>
+                            Ticker
+                        </StyledTableCell>
+                        <StyledTableCell isHeader key={`ticker_header_1`}>
+                            Change (%)
+                        </StyledTableCell>
+                        <StyledTableCell isHeader key={`ticker_header_2`}>
+                            Last Close
+                        </StyledTableCell>
+                        <StyledTableCell width="30%" isHeader key={`ticker_header_2`}>
+                            Chart
+                        </StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {tickerData?.map(entry => {
+                        return (
+                            <StyledTableRow>
+                                <StyledTableCell>{entry.ticker}</StyledTableCell>
+                                <StyledTableCell color= {entry.pct_change > 0 ? ColorsEnum.upHint : ColorsEnum.downHint }>{entry.pct_change.toFixed(2)}%</StyledTableCell>
+                                <StyledTableCell>{entry.last_close}</StyledTableCell>
+                                <TableCellWrapper id={`${entry.ticker}_tickerChartWrapper`}>
+                                    <BaseLineChart
+                                        baseId={`${entry.ticker}_tickerChart`}
+                                        defaultData={entry.chartData}
+                                        width={100}
+                                        height={30}
+                                        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                                    />
+                                </TableCellWrapper>
+                            </StyledTableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </WidgetContainer>
+    );
+}
