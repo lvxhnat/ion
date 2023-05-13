8e584c0ec40bc20dba31e1d0c8248ad6083fe4d0
lvxhnat <yikuang5@gmail.com>
2023-05-13 18:49:02 +0800

    init: geopolitics

--- ion-frontend/src/pages/geopolitics/geopolitics.tsx
+++ ion-frontend/src/pages/geopolitics/geopolitics.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import * as React from 'react'
+
+import CssBaseline from '@mui/material/CssBaseline'
+import Navigation from 'components/Navigation'
 
 export default function Geopolitics() {
   return (
-    <div>geopolitics</div>
+    <div>
+      <CssBaseline />
+      <Navigation />
+    </div>
   )
 }
