0f030008a769947c2ed416bc6eff6fa2a2aca984
lvxhnat <yikuang5@gmail.com>
2023-05-25 13:42:34 +0800

    edit: cli

--- ion-ingestion/src/ion/cli/provider_cli.py
+++ ion-ingestion/src/ion/cli/provider_cli.py
@@ -1,21 +1,23 @@
 import click
 import subprocess
 from pathlib import Path
-from sqlalchemy import
+
 
 @click.group()
 def provider() -> None:
     """Helper Commands for setting up and interacting with Ion Provider Instance."""
     pass
 
-@click.command() 
+
+@click.command()
 def preinitialise():
     ## A step that we run before initialising the prefect servers. This instantiates any missing tables in our postgres DB and runs the necessary flows
     pass
 
+
 @click.command()
 def deploy():
-    """ Deploy any code changes directly onto the prefect agent server """
+    """Deploy any code changes directly onto the prefect agent server"""
     DEPLOYMENT_FILE_PATH = Path(__file__).parents[2] / "ion_provider" / "deployment.py"
     subprocess.run(["python", DEPLOYMENT_FILE_PATH])
 
