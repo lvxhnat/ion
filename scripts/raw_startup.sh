964a892be0ca926e86a78d54843ae23599b577bb
lvxhnat <yikuang5@gmail.com>
2023-09-29 13:18:34 +0200

    clean up directories

--- scripts/raw_startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export $(cat .env | xargs)
-prefect orion start &
-prefect agent start -q "treasury" "common" &
-echo $SUDO_PASSWORD | sudo -S kill -9 $($SUDO_PASSWORD | sudo -S lsof -i:5432 | awk '$8 == "TCP" { print $2 }') &
-docker compose -f "Users/lohyikuang/Downloads/personal_projects/ion/docker/docker-compose.base.yml" up &
-brew services start rabbitmq 
-
