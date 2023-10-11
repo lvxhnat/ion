93714482fa4884004f9858cdece935ef01420bff
lvxhnat <yikuang5@gmail.com>
2023-10-11 08:42:07 +0200

    edit: formatting

--- finflow-frontend/src/components/Navigator/Navigator.tsx
+++ finflow-frontend/src/components/Navigator/Navigator.tsx
@@ -6,14 +6,15 @@ export default function Navigator() {
   return (
     <Toolbar>
       <AppBar>
-        <Grid container style={{ padding: 10 }}>
+        <Grid container padding={1}>
           <Grid item xs={6}></Grid>
           <Grid item xs={6}>
             <Box
               display="flex"
               justifyContent="flex-end"
               alignItems="center"
-              gap={3}
+              gap={2}
+              paddingRight={2}
             >
               <ToggleThemeMode />
             </Box>
--- finflow-frontend/src/components/Navigator/ToggleThemeMode/ToggleThemeMode.tsx
+++ finflow-frontend/src/components/Navigator/ToggleThemeMode/ToggleThemeMode.tsx
@@ -18,7 +18,7 @@ function ToggleThemeMode() {
       onClick={handleClick}
       disableRipple
       color="inherit"
-      style={{ border: "1px solid white", width: 35, height: 35 }}
+      style={{ border: "1px solid white", width: 30, height: 30 }}
     >
       {mode === "dark" ? (
         <Brightness7Icon fontSize="small" data-testid="theme-mode-light-id" />
