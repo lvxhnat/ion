4e563b46ad550639495522561e3daa478ea13f14
Loh Yi Kuang <yikuang5@gmail.com>
2024-02-24 00:43:30 +0800

    edit: docker network

---
+++ frontend/src/pages/About/style.ts
@@ -0,0 +1,8 @@
+import { styled } from '@mui/system';
+
+export const AboutWrapper = styled('div')(({ theme }) => ({
+    width: '100%',
+    height: '100%',
+    overflowY: 'scroll',
+    '&::-webkit-scrollbar': { display: 'none' },
+}));
