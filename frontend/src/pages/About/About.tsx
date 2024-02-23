3af0d166ddf2c9d133db31029f61930aa9d29671
Loh Yi Kuang <yikuang5@gmail.com>
2024-02-23 14:36:07 +0800

    add: about const

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -3,6 +3,16 @@ import { Grid, Typography } from '@mui/material';
 import { ContainerWrapper } from 'components/Wrappers/ContainerWrapper';
 import Typewriter from 'typewriter-effect';
 
+const TEXT = {
+  SLOGAN: 'Streamlining Insights, Powering Decisions',
+  ABOUT: `
+        FinFlow originated three years ago as a side project to tackle the challenge of fragmented economic data, eventually evolving into a premier platform centralizing alternative, 
+        open-source data from sourcs such as FRED. Designed to democratize access to economic information, FinFlow aims to empowers users of all backgrounds to make informed decisions with ease. 
+        By amalgamating diverse data sets into a user-friendly dashboard, we offer clarity and actionable insights for financial and general decision-making alike. 
+        `,
+  
+}
+
 export default function About() {
   return (
     <ContainerWrapper>
@@ -10,7 +20,7 @@ export default function About() {
       <Typography variant="h1">
       <Typewriter
         options={{
-          strings: ['Streamlining Insights, Powering Decisions'],
+          strings: [TEXT.SLOGAN],
           autoStart: true,
           loop: true,
           deleteSpeed: 10
@@ -20,9 +30,7 @@ export default function About() {
       </Grid>
       <Grid container style={{ padding: 100 }}>
         <Typography variant="subtitle1" align="center">
-        FinFlow originated three years ago as a side project to tackle the challenge of fragmented economic data, eventually evolving into a premier platform centralizing alternative, 
-        open-source data from sourcs such as FRED. Designed to democratize access to economic information, FinFlow aims to empowers users of all backgrounds to make informed decisions with ease. 
-        By amalgamating diverse data sets into a user-friendly dashboard, we offer clarity and actionable insights for financial and general decision-making alike. 
+          {TEXT.ABOUT}
         </Typography>
       </Grid>
     </ContainerWrapper>
